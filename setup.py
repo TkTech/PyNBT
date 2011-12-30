d3b91d089f3b9b0853bf67cbf279b2309cb5c28a
Tyler Kennedy <tk@tkte.ch>
2011-12-30 11:53:42 -0500

    Added classifiers and description.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='OpenNBT',
     version='0.0.1',
-    description='',
+    description='Tiny, liberally-licensed NBT library. NBT is the datafile format used in Minecraft.',
     author='Tyler Kennedy',
     author_email='tk@tkte.ch',
     url='https://github.com/TkTech/OpenNBT',
@@ -14,5 +14,11 @@ setup(
     ],
     scripts=[
         'scripts/debug-nbt'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License'
     ]
 )
